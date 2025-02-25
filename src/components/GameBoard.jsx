de47ff651c5ac4229dcb6ef11406ecbd7971d051
Aaron Weidele <aweidele@gmail.com>
2025-02-25 14:11:08 -0500

    buttons

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -37,27 +37,22 @@ export function GameBoard() {
   };
 
   return (
-    <>
-      {/* <div>
-        <input type="number" value={buttonCount} onChange={handleButtonCount} />
-      </div> */}
-      <div className="max-w-210 max-h-screen mx-auto p-5">
-        <div className="h-full w-full bg-almost-black rounded-full aspect-square relative">
-          {gameButtons.map((btn, i) => (
-            <GameButton
-              key={btn.color}
-              color={btn.color}
-              active={activeButton === i + 1}
-              onMouseDown={() => {
-                handleButtonDown(i + 1);
-              }}
-              onMouseUp={handleButtonUp}
-              position={btn.position}
-              percent={1 / buttonCount}
-            />
-          ))}
-        </div>
+    <div className="max-w-210 max-h-screen mx-auto p-5">
+      <div className="h-full w-full bg-almost-black rounded-full aspect-square relative">
+        {gameButtons.map((btn, i) => (
+          <GameButton
+            key={btn.color}
+            color={btn.color}
+            active={activeButton === i + 1}
+            onMouseDown={() => {
+              handleButtonDown(i + 1);
+            }}
+            onMouseUp={handleButtonUp}
+            position={btn.position}
+            percent={1 / buttonCount}
+          />
+        ))}
       </div>
-    </>
+    </div>
   );
 }
